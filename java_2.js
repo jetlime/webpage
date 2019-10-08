6ea644300976c220c3e662e9c7205292725399d9
jetlime <paul.houssel@yahoo.de>
2019-10-08 10:26:09 +0200

    debugging

--- java_2.js
+++ java_2.js
@@ -4,52 +4,50 @@ var test=[{
     time : 0.03,
     pass_fail: 20,
 },{
-    "pass": 10,
-    "fail" : 0,
-    "time" : 0.09, 
-    "pass_fail": 100,
+    pass: 10,
+    fail : 0,
+    time : 0.09, 
+    pass_fail: 100
+},{
+    pass: 15,
+    fail : 0,
+    time : 0.04, 
+    pass_fail: 100,
+},{
+    pass: 119,
+    fail : 2,
+    time : 0.04, 
+    pass_fail: 0
 }];
 // implementing the dynamic length of the object into the tittle :
 document.getElementById("table_javascript").innerHTML = `
 <h2 class="text"> Test Statistics (${test.length} results) :</h2>
+<div class='table_1'>
+<table>
+<thead>
+    <tr>
+        <th>Total</th>
+        <th>Pass</th>
+        <th>Fail</th>
+        <th>Time needed</th>
+        <th>Pass/Fail ratio</th>
+    </tr>
+</thead>
+<tbody>
 ${test.map(function(parameter) {
     return `
-    <div class='table_1'>
-    <table>
-    <thead>
-        <tr>
-            <th>Total</th>
-            <th>Pass</th>
-            <th>Fail</th>
-            <th>Time needed</th>
-            <th>Pass/Fail ratio</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-           <th>Test 1</th> 
-           <th>${test.pass}</th>
-           <th>${test.fail}</th>
-           <th>${test.time}</th>
-           <th>${test.pass_fail}</th>
-        </tr>
-        <tr>
-            <th>Test 2</th>
-            <th>Yes</th>
-            <th>NO</th>
-            <th>3.9s</th>
-            <th>90%</th>
-        </tr>
-        <tr>
 
-
+        <tr>
+           <th>${parameter.}</th> 
+           <th>${parameter.pass}</th>
+           <th>${parameter.fail}</th>
+           <th>${parameter.time}</th>
+           <th>${parameter.pass_fail}</th>
         </tr>
-
-    </tbody>
-</table>
     </div>`;
 }).join("")}
-
+</tbody>
+</table>
 `
  
 
