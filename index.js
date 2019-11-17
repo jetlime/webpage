5de018231de2fcc1c914614382b29ec2b6b81522
jetlime <paul.houssel@yahoo.de>
2019-11-17 16:40:19 +0100

    debug

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const morgan = require("morgan");
 //will give use the time needed for the request (debug help)
 app.use(morgan("short"));
-app.use("/api", express.static("api"), function (req, res) {
-  res.status(404);
-  res.json({ error: { code: 400 } });
+app.use("/api", express.static("api"), function(req, res) {
+    res.status(404);
+    res.json({ error: { code: 400 } });
 });
 /*
 var cors = require('cors');
@@ -28,87 +28,86 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // Json Arrays :
-const test = [
-  {
-    id: 0,
-    test_id: "password test",
-    pass: 1,
-    fail: 5,
-    time: 0.03,
-    pass_fail: 20
-  },
-  {
-    id: 1,
-    test_id: "password test",
-    pass: 1,
-    fail: 5,
-    time: 0.03,
-    pass_fail: 20
-  },
-  {
-    id: 2,
-    test_id: "Form testing",
-    pass: 10,
-    fail: 0,
-    time: 0.09,
-    pass_fail: 100
-  },
-  {
-    id: 3,
-    test_id: "Login Test 2",
-    pass: 15,
-    fail: 0,
-    time: 0.04,
-    pass_fail: 100
-  },
-  {
-    id: 4,
-    test_id: "Pen Test",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 0
-  },
-  {
-    id: 5,
-    test_id: "Pen Test 2",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 1
-  },
-  {
-    id: 6,
-    test_id: "Pen Test 3",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 40
-  },
-  {
-    id: 7,
-    test_id: "Login Test 4",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 0
-  },
-  {
-    id: 8,
-    test_id: "Pen Test 4",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 80
-  },
-  {
-    id: 9,
-    test_id: "Pen Test 4",
-    pass: 119,
-    fail: 2,
-    time: 0.04,
-    pass_fail: 0
-  }
+const test = [{
+        id: 0,
+        test_id: "password test",
+        pass: 1,
+        fail: 5,
+        time: 0.03,
+        pass_fail: 20
+    },
+    {
+        id: 1,
+        test_id: "password test",
+        pass: 1,
+        fail: 5,
+        time: 0.03,
+        pass_fail: 20
+    },
+    {
+        id: 2,
+        test_id: "Form testing",
+        pass: 10,
+        fail: 0,
+        time: 0.09,
+        pass_fail: 100
+    },
+    {
+        id: 3,
+        test_id: "Login Test 2",
+        pass: 15,
+        fail: 0,
+        time: 0.04,
+        pass_fail: 100
+    },
+    {
+        id: 4,
+        test_id: "Pen Test",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 0
+    },
+    {
+        id: 5,
+        test_id: "Pen Test 2",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 1
+    },
+    {
+        id: 6,
+        test_id: "Pen Test 3",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 40
+    },
+    {
+        id: 7,
+        test_id: "Login Test 4",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 0
+    },
+    {
+        id: 8,
+        test_id: "Pen Test 4",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 80
+    },
+    {
+        id: 9,
+        test_id: "Pen Test 4",
+        pass: 119,
+        fail: 2,
+        time: 0.04,
+        pass_fail: 0
+    }
 ];
 
 // Get request by going to the root (/) :
@@ -116,33 +115,34 @@ const test = [
 // "req" is the request from the browser.
 // "res" is the response from the server.
 app.get("/", (req, res) => {
-  res.send(
-    'This is the root of the local webserver. Under the directory "/test" you will find the Json arrays.'
-  );
+    res.send(
+        'This is the root of the local webserver. Under the directory "/test" you will find the Json arrays.'
+    );
 });
 
 // Get the Json array in the following directory :
 app.get("/test", (req, res) => {
-  console.log(test);
-  res.jsonp({ test: test });
+    console.log(test);
+    res.jsonp({ test: test });
 });
 
 // Get a single test :
 app.get("/test/:id", (req, res) => {
-  // If the id does not exist an error is shown :
-  if (req.params.id > test.length - 1) {
-    res
-      .status(404)
-      .send(
-        "ERROR 404 File not found. A test with an id of " +
-        req.params.id +
-        " does not exist."
-      );
-    console.log("This id does not exist.");
-  } else {
-    console.log("Fetching user with id " + req.params.id);
-    //res.json(test[req.params.id]);
-    res.send(`
+    // If the id does not exist an error is shown :
+    if (req.params.id > test.length - 1) {
+        res
+            .status(404)
+            .send(
+                "ERROR 404 File not found. A test with an id of " +
+                req.params.id +
+                " does not exist."
+            );
+        console.log("This id does not exist.");
+    } else {
+        console.log("Fetching user with id " + req.params.id);
+        //res.json(test[req.params.id]);
+        console.log(JSON.stringify(test[req.params.id]))
+        res.send(`
     <style>
     .idpage{
       color : #4CAF50;
@@ -180,12 +180,15 @@ app.get("/test/:id", (req, res) => {
     color: black;
     background-color: #dddddd;
 }
-
+.myProgress {
+  width: 180%;
+  background-color: #ddd;
+}
     </style>
-  ${test}
+  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <div class="idpage"></div>
+    <div class="idpage">${JSON.stringify(test[req.params.id].test_id).slice(1,-1).toUpperCase()}</div>
         <br>
         <hr width="60%" size="2" color="#4CAF50">
         <br>
@@ -195,23 +198,23 @@ app.get("/test/:id", (req, res) => {
                 <div class="col-sm">
                     <h2>Average time : </h2>
                     <br>
-                     seconds
+                    ${JSON.stringify(test[req.params.id].time)} seconds
                 </div>
                 <div class="col-sm">
                     <h2>Passed tests :</h2>
                     <br>
-                    
+                    ${JSON.stringify(test[req.params.id].pass)}
                 </div>
                 <div class="col-sm">
-                    <h2>Failed Test :</h2>
+                    <h2>Failed Test : </h2>
                     <br>
-                    
+                    ${JSON.stringify(test[req.params.id].fail)}
                 </div>
                 <div class="col-sm">
                     <h2>Ratio :</h2>
                     <br>
                     <div class='myProgress'>
-                    <div style="background-color:#4CAF50; width:%;">%</div>
+                    <div style="background-color:#4CAF50; width:${JSON.stringify(test[req.params.id].pass_fail)}%;">${JSON.stringify(test[req.params.id].pass_fail)}%</div>
                     </div>
                 </div>
             </div>
@@ -244,5 +247,5 @@ app.get("/test/:id", (req, res) => {
     
     
     `)
-  }
+    }
 });
